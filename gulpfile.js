13ebc14d31a5a88f95a05af71ee5b71f07f500a0
ZaphodBB <eigentum@me.com>
2026-02-18 11:26:56 -0500

    Changed CSS minifier

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulpit = require("gulp");
-const cleancss = require('gulp-clean-css');
+//const cleancss = require('gulp-clean-css');
 const csscomb = require('gulp-csscomb');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
-const concat = require("gulp-concat")
+const concat = require("gulp-concat");
+const csso = require('gulp-csso');
 
 function buildCore() {
     console.log("Building Core");
@@ -17,14 +18,14 @@ function buildCore() {
         .pipe(concat("goldilocks.core.css"))
         .pipe(csscomb())
         .pipe(gulpit.dest('./dist'))
-        .pipe(cleancss())
+        .pipe(csso())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(gulpit.dest('./dist'));
 }
 
-function buildElements() {
+function buildCoreElements() {
     console.log("Building Elements");
     return gulpit
         .src([
@@ -34,10 +35,10 @@ function buildElements() {
             './css/core/core-elements/*.css'
         ])
         //.pipe(autoprefixer())
-        .pipe(concat("goldilocks.elements.css"))
+        .pipe(concat("goldilocks.core-elements.css"))
         .pipe(csscomb())
         .pipe(gulpit.dest('./dist'))
-        .pipe(cleancss())
+        .pipe(csso())
         .pipe(rename({
             suffix: '.min'
         }))
@@ -52,7 +53,7 @@ function buildExtended() {
         .pipe(concat("goldilocks.extended.css"))
         .pipe(csscomb())
         .pipe(gulpit.dest('./dist'))
-        .pipe(cleancss())
+        .pipe(csso())
         .pipe(rename({
             suffix: '.min'
         }))
@@ -67,7 +68,7 @@ function buildExtras() {
         .pipe(concat("goldilocks.extras.css"))
         .pipe(csscomb())
         .pipe(gulpit.dest('./dist'))
-        .pipe(cleancss())
+        .pipe(csso())
         .pipe(rename({
             suffix: '.min'
         }))
@@ -80,15 +81,15 @@ function buildCoreExtended() {
         .src([
             './css/core/base/*.css',
             './css/core/core-layout/*.css',
+            './css/core/core-helpers/*.css',
             './css/core/core-elements/*.css',
-            './css/extended/**/*.css',
-            './css/core/core-helpers/*.css'
+            './css/extended/**/*.css'
         ])
         //.pipe(autoprefixer())
         .pipe(concat("goldilocks.core-extended.css"))
         .pipe(csscomb())
         .pipe(gulpit.dest('./dist'))
-        .pipe(cleancss())
+        .pipe(csso())
         .pipe(rename({
             suffix: '.min'
         }))
@@ -110,7 +111,7 @@ function buildAll() {
         .pipe(concat("goldilocks.all.css"))
         .pipe(csscomb())
         .pipe(gulpit.dest('./dist'))
-        .pipe(cleancss())
+        .pipe(csso())
         .pipe(rename({
             suffix: '.min'
         }))
@@ -124,9 +125,9 @@ function watch() {
 
 //exports.watch = watch;
 exports.buildCore = buildCore;
-exports.buildElements = buildElements;
-exports.buildExtended = buildExtended;
-exports.buildExtras = buildExtras;
+exports.buildCoreElements = buildCoreElements;
+//exports.buildExtended = buildExtended;
+//exports.buildExtras = buildExtras;
 exports.buildCoreExtended = buildCoreExtended;
 exports.buildAll = buildAll;
 exports.default = buildAll;
